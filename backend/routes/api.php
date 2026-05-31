863fc8a529e6aaea84c687443798857fdeb2e5fc
Nicolas Kleiton <inicolaskleiton@gmail.com>
2026-05-31 16:15:19 -0300

    chore(routes): agrupar rotas sob o middleware de autenticacao sanctum

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -8,6 +8,14 @@ Route::post("/register", [AuthController::class,"register"]);
 
 Route::post('/login', [AuthController::class, 'login']);
 
-Route::get('/movies/popular', [MovieController::class, 'getPopular']);
-
-Route::get('/movies/search', [MovieController::class, 'search']);
+Route::middleware('auth:sanctum')->group(function () {
+    
+    // Catálogo e Busca de Filmes
+    Route::get('/movies/popular', [MovieController::class, 'getPopular']);
+    Route::get('/movies/search', [MovieController::class, 'search']);
+    
+    // Gerenciamento do Histórico de Sessões
+    Route::post('/movie-sessions', [MovieController::class, 'store']);
+    Route::get('/movie-sessions/history', [MovieController::class, 'history']);
+    
+});
