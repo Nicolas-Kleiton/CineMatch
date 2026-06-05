275a599b55afd1b339523e9b2a26aebab65040a4
Nicolas Kleiton <inicolaskleiton@gmail.com>
2026-06-04 21:58:01 -0300

    fix(routes): protege rotas privadas e corrige redirecionamento padrao

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -5,20 +5,24 @@ import { Dashboard } from './pages/dashboard/dashboard';
 import { authGuard } from './guards/auth-guard';
 
 export const routes: Routes = [
+    { path: '', redirectTo: 'login', pathMatch: 'full' },
     { path: 'register', component: Register},
     { path: 'login', component: Login },
     {
-    path: 'dashboard',
-    loadComponent: () => import('./pages/dashboard/dashboard').then(m => m.Dashboard)
+        path: 'dashboard',
+        loadComponent: () => import('./pages/dashboard/dashboard').then(m => m.Dashboard),
+        canActivate: [authGuard]
     },
     {
-    path: 'history',
-    loadComponent: () => import('./pages/history/history').then(m => m.History)
+        path: 'history',
+        loadComponent: () => import('./pages/history/history').then(m => m.History),
+        canActivate: [authGuard]
     },
     {
-    path: 'profile',
-    loadComponent: () => import('./pages/profile/profile').then(m => m.Profile)
+        path: 'profile',
+        loadComponent: () => import('./pages/profile/profile').then(m => m.Profile),
+        canActivate: [authGuard]
     },
 
-    { path: '**', redirectTo: 'dashboard' }
+    { path: '**', redirectTo: 'login' }
 ];
